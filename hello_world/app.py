8fd0bb02f57b9f77eeb26b9622016c349215b617
amalvas-git <80445320+amalvas-git@users.noreply.github.com>
2021-04-15 15:55:50 +0530

    Update app.py

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
+            "message": "hello world on Apr 15 2021",
             # "location": ip.text.replace("\n", "")
         }),
     }
