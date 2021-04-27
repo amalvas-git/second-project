90cf289bc1aec07a116067939a9c1007eac8e3bc
amalvas-git <80445320+amalvas-git@users.noreply.github.com>
2021-04-27 21:05:01 +0530

    Update test_script.py

--- hello_world/test/test_script.py
+++ hello_world/test/test_script.py
@@ -1,18 +1,18 @@
 import unittest
 from hello_world.app import lambda_handler
 
-class SimpleTest(unittest.TestCase):
+#class SimpleTest(unittest.TestCase):
 			
 	#def test_message(self):
 	#	result=lambda_handler()
 	#	self.assertEqual(result, "hellow world")
-	def test_lambda_handler(apigw_event, mocker):
+def test_lambda_handler(apigw_event, mocker):
 
-    		ret = app.lambda_handler(apigw_event, "")
-    		data = json.loads(ret["body"])
-    		assert ret["statusCode"] == 200
-    		assert "message" in ret["body"]
-    		assert data["message"] == "hello world"
+    	ret = app.lambda_handler(apigw_event, "")
+    	data = json.loads(ret["body"])
+  	assert ret["statusCode"] == 200
+    	assert "message" in ret["body"]
+	assert data["message"] == "hello world"
 
 if __name__ == '__main__':
 	unittest.main()
