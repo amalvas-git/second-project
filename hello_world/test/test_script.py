90cae145c73d84e60d889ca53cd28f2c0358ef41
amalvas-git <80445320+amalvas-git@users.noreply.github.com>
2021-04-27 21:18:13 +0530

    Update test_script.py

--- hello_world/test/test_script.py
+++ hello_world/test/test_script.py
@@ -7,12 +7,11 @@ class SimpleTest(unittest.TestCase):
 	#	result=lambda_handler()
 	#	self.assertEqual(result, "hellow world")
 	def test_lambda_handler(apigw_event):
-
-    		ret = app.lambda_handler(apigw_event, "")
+		ret = lambda_handler(apigw_event, "")
     		data = json.loads(ret["body"])
   		#assert ret["statusCode"] == 200
-    		assert "message" in ret["body"]
-		assert data["message"] == "hello world"
+		assert "message" in ret["body"]
+		assert data["message"]== "hello world"
 
 if __name__ == '__main__':
 	unittest.main()
